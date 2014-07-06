4eb287ad34c6caea250bfcc30fd09ff045393314
miles <miles@hamster>
2014-07-05 23:27:02 -0700

    Bringing the hamLibs PRNG into the local namespace.

---
+++ include/lsRandom.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   lsRandom.h
+ * Author: miles
+ *
+ * Created on July 5, 2014, 10:00 PM
+ */
+
+#ifndef __LS_RANDOM_H__
+#define	__LS_RANDOM_H__
+
+#include "utils/randomNum.h"
+
+// bringing the hamLibs prng into the local namespace.
+typedef hamLibs::utils::randomNum lsRandom;
+
+#endif	/* LSRANDOM_H */
+
---
+++ src/lsRandom.cpp
@@ -0,0 +1,9 @@
+/* 
+ * File:   lsRandom.cpp
+ * Author: miles
+ * 
+ * Created on July 5, 2014, 10:00 PM
+ */
+
+#include "lsRandom.h"
+

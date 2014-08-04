0ac38a25a93d36d9e767a9d592cec2a8adca46fc
miles <miles@hamster>
2014-08-03 18:44:35 -0700

    Added more texture format/color types.

--- include/lsColor.h
+++ include/lsColor.h
@@ -41,6 +41,7 @@ enum ls_color_type_t : int {
  */
 enum ls_pixel_layout_t : int {
     LS_GRAY             = GL_DEPTH_COMPONENT,
+    LS_R                = GL_RED,
     LS_RG               = GL_RG,
     LS_RGB              = GL_BGR,
     LS_RGBA             = GL_BGRA,
@@ -56,39 +57,56 @@ enum ls_pixel_layout_t : int {
  */
 enum ls_pixel_format_t : int {
     LS_GRAY_8           = GL_DEPTH_COMPONENT,
+    LS_R_8              = GL_R8,
     LS_RG_8             = GL_RG8,
     LS_RGB_8            = GL_RGB8,
     LS_RGBA_8           = GL_RGBA8,
     
     LS_GRAY_16I         = GL_DEPTH_COMPONENT16,
+    LS_R_16I            = GL_R16I,
     LS_RG_16I           = GL_RG16I,
     LS_RGB_16I          = GL_RGB16I,
     LS_RGBA_16I         = GL_RGBA16I,
     
     LS_GRAY_16U         = GL_DEPTH_COMPONENT16,
+    LS_R_16U            = GL_R16UI,
     LS_RG_16U           = GL_RG16UI,
     LS_RGB_16U          = GL_RGB16UI,
     LS_RGBA_16U         = GL_RGBA16UI,
     
     LS_GRAY_32I         = GL_DEPTH_COMPONENT32,
+    LS_R_32I            = GL_R32I,
     LS_RG_32I           = GL_RG32I,
     LS_RGB_32I          = GL_RGB32I,
     LS_RGBA_32I         = GL_RGBA32I,
     
     LS_GRAY_32U         = GL_DEPTH_COMPONENT32,
+    LS_R_32U            = GL_R32UI,
     LS_RG_32U           = GL_RG32UI,
     LS_RGB_32U          = GL_RGB32UI,
     LS_RGBA_32U         = GL_RGBA32UI,
     
     LS_GRAY_16F         = GL_R16F,
+    LS_R_8F             = GL_R16F,
     LS_RG_16F           = GL_RG16F,
     LS_RGB_16F          = GL_RGB16F,
     LS_RGBA_16F         = GL_RGBA16F,
     
     LS_GRAY_32F         = GL_DEPTH_COMPONENT32F,
+    LS_R_32F            = GL_R32F,
     LS_RG_32F           = GL_RG32F,
     LS_RGB_32F          = GL_RGB32F,
     LS_RGBA_32F         = GL_RGBA32F,
+    
+    LS_SRGB             = GL_SRGB,
+    LS_SRGB_8           = GL_SRGB8,
+    LS_SRGBA            = GL_SRGB_ALPHA,
+    LS_SRGBA_8          = GL_SRGB8_ALPHA8,
+    
+    LS_R_C              = GL_COMPRESSED_RED,
+    LS_RG_C             = GL_COMPRESSED_RG,
+    LS_RGB_C            = GL_COMPRESSED_RGB,
+    LS_RGBA_C           = GL_COMPRESSED_RGBA,
     
     LS_DEFAULT_PIXEL_FORMAT = GL_RGB,
     LS_INVALID_PIXEL_FORMAT = -1

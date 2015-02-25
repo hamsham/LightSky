c297ee336a8d861575482ff3b001ac735e158d0a
hammy <hammy@hambox>
2015-02-25 01:20:27 -0800

    Beginning skeletal animations.

---
+++ include/lightsky/draw/bone.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   bone.h
+ * Author: hammy
+ *
+ * Created on February 18, 2015, 10:28 PM
+ */
+
+#ifndef BONE_H
+#define	BONE_H
+
+class bone {
+public:
+    bone();
+    bone(const bone& orig);
+    virtual ~bone();
+private:
+
+};
+
+#endif	/* BONE_H */
+
---
+++ src/draw/bone.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   bone.cpp
+ * Author: hammy
+ * 
+ * Created on February 18, 2015, 10:28 PM
+ */
+
+#include "lightsky/draw/bone.h"
+
+bone::bone() {
+}
+
+bone::bone(const bone&) {
+}
+
+bone::~bone() {
+}
+

efe8da85af0d53d2f66484f16cf21aeff8d18605
hammy <hammy@hambox>
2015-03-23 02:50:11 -0700

    Updating build - 20150322

--- LightMath/nbproject/Makefile-variables.mk
+++ LightMath/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-Linux-x86
+CND_PLATFORM_Debug=CLang-Linux-x86
 CND_ARTIFACT_DIR_Debug=build
 CND_ARTIFACT_NAME_Debug=liblightmath_d.a
 CND_ARTIFACT_PATH_Debug=build/liblightmath_d.a
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_DIR_Debug=dist/Debug/CLang-Linux-x86/package
 CND_PACKAGE_NAME_Debug=LightMath.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/LightMath.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/CLang-Linux-x86/package/LightMath.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux-x86
+CND_PLATFORM_Release=CLang-Linux-x86
 CND_ARTIFACT_DIR_Release=build
 CND_ARTIFACT_NAME_Release=liblightmath.a
 CND_ARTIFACT_PATH_Release=build/liblightmath.a
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_DIR_Release=dist/Release/CLang-Linux-x86/package
 CND_PACKAGE_NAME_Release=LightMath.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/LightMath.tar
+CND_PACKAGE_PATH_Release=dist/Release/CLang-Linux-x86/package/LightMath.tar
 #
 # include compiler specific variables
 #
--- LightSetup/nbproject/Makefile-variables.mk
+++ LightSetup/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-Linux-x86
+CND_PLATFORM_Debug=CLang-Linux-x86
 CND_ARTIFACT_DIR_Debug=build
 CND_ARTIFACT_NAME_Debug=liblightsetup_d.a
 CND_ARTIFACT_PATH_Debug=build/liblightsetup_d.a
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_DIR_Debug=dist/Debug/CLang-Linux-x86/package
 CND_PACKAGE_NAME_Debug=LightSetup.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/LightSetup.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/CLang-Linux-x86/package/LightSetup.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux-x86
+CND_PLATFORM_Release=CLang-Linux-x86
 CND_ARTIFACT_DIR_Release=build
 CND_ARTIFACT_NAME_Release=liblightsetup.a
 CND_ARTIFACT_PATH_Release=build/liblightsetup.a
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_DIR_Release=dist/Release/CLang-Linux-x86/package
 CND_PACKAGE_NAME_Release=LightSetup.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/LightSetup.tar
+CND_PACKAGE_PATH_Release=dist/Release/CLang-Linux-x86/package/LightSetup.tar
 #
 # include compiler specific variables
 #

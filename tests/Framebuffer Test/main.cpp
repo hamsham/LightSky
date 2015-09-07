61899bb31ec538533a03a5b1a8b1bd57527f992a
hamsham <mileslacey@gmail.com>
2015-09-06 20:14:46 -0700

    Fixed a bug preventing an OpenGL context from being initialized with the version required by LightSky.

--- tests/Framebuffer Test/main.cpp
+++ tests/Framebuffer Test/main.cpp
@@ -152,22 +152,6 @@ bool initSubsystems() {
     
     SDL_SetMainReady();
     
-    /*
-     * Setup the necessary parameters for OpenGL 3.3
-     */
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,
-        SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG
-#ifdef LS_DEBUG
-        | SDL_GL_CONTEXT_DEBUG_FLAG
-#endif
-    );
-    
     constexpr uint32_t sdlInitFlags = 0
         | SDL_INIT_TIMER
         | SDL_INIT_AUDIO
@@ -187,6 +171,22 @@ bool initSubsystems() {
     LS_LOG_MSG(
         "Successfully initialized SDL.\n",
         SDL_GetError(), '\n'
+    );
+    
+    /*
+     * Setup the necessary parameters for OpenGL 3.3
+     */
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,
+        SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG
+#ifdef LS_DEBUG
+        | SDL_GL_CONTEXT_DEBUG_FLAG
+#endif
     );
     SDL_ClearError();
     

08ee388e1102ccb5cfed0e9bc1ee0460de663183
hammy <mileslacey@gmail.com>
2017-01-27 19:28:51 -0800

    Attempting to correct issues with OSX build.

--- tci_osx_setup.bash
+++ tci_osx_setup.bash
@@ -7,9 +7,9 @@ set -ex
 brew update
 brew upgrade
 
-brew outdated sdl2      || brew unlink sdl2      || brew install sdl2
-brew outdated freeimage || brew unlink freeimage || brew install freeimage
-brew outdated assimp    || brew unlink assimp    || brew install assimp
-brew outdated freetype  || brew unlink freetype  || brew install freetype
-brew outdated boost     || brew unlink boost     || brew install boost
-brew outdated glm       || brew unlink glm       || brew install glm
+brew outdated sdl2      || brew unlink sdl2      || 0; brew install sdl2
+brew outdated freeimage || brew unlink freeimage || 0; brew install freeimage
+brew outdated assimp    || brew unlink assimp    || 0; brew install assimp
+brew outdated freetype  || brew unlink freetype  || 0; brew install freetype
+brew outdated boost     || brew unlink boost     || 0; brew install boost
+brew outdated glm       || brew unlink glm       || 0; brew install glm
